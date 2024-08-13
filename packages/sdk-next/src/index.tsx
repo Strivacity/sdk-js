15d4019c70f930a2286b174f863037b9d14fedd1
Péter Gaál <peter.gaal@strivacity.com>
2024-08-13 22:06:32 +0200

    feat: @strivacity/sdk-next package implemented

---
+++ packages/sdk-next/src/index.tsx
@@ -0,0 +1,113 @@
+import { type FC, createContext, useContext, useMemo, useEffect, useState } from 'react';
+import { initFlow, type SDKOptions, type SDKStorage, type IdTokenClaims } from '@strivacity/sdk-core';
+import type { PopupFlow } from '@strivacity/sdk-core/flows/PopupFlow';
+import type { RedirectFlow } from '@strivacity/sdk-core/flows/RedirectFlow';
+import { LocalStorage } from '@strivacity/sdk-core/storages/LocalStorage';
+import { SessionStorage } from '@strivacity/sdk-core/storages/SessionStorage';
+import type { Session, PopupContext, PopupSDK, RedirectContext, RedirectSDK, Children } from './types';
+
+export type { SDKOptions, SDKStorage, Session, IdTokenClaims, PopupFlow, RedirectFlow, PopupContext, PopupSDK, RedirectContext, RedirectSDK };
+export { LocalStorage, SessionStorage };
+
+const StrivacitySdk = createContext<PopupContext | RedirectContext>(null!);
+
+let sdk: RedirectFlow | PopupFlow;
+
+export const useStrivacity = <T extends PopupContext | RedirectContext>() => {
+	const context = useContext(StrivacitySdk);
+
+	if (!context) {
+		throw Error('Missing Strivacity SDK context');
+	}
+
+	return context as T;
+};
+
+export const AuthProvider: FC<{ options: SDKOptions; children?: Children }> = ({
+	options,
+	children = undefined,
+}: {
+	options: SDKOptions;
+	children?: Children;
+}) => {
+	const [loading, setLoading] = useState<boolean>(true);
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+	const [idTokenClaims, setIdTokenClaims] = useState<IdTokenClaims | null>(null);
+	const [accessToken, setAccessToken] = useState<string | null>(null);
+	const [refreshToken, setRefreshToken] = useState<string | null>(null);
+	const [accessTokenExpired, setAccessTokenExpired] = useState<boolean>(true);
+	const [accessTokenExpirationDate, setAccessTokenExpirationDate] = useState<number | null>(null);
+
+	const updateSession = async () => {
+		setIsAuthenticated(await sdk.isAuthenticated);
+		setIdTokenClaims(sdk.idTokenClaims || null);
+		setAccessToken(sdk.accessToken || null);
+		setRefreshToken(sdk.refreshToken || null);
+		setAccessTokenExpired(sdk.accessTokenExpired);
+		setAccessTokenExpirationDate(sdk.accessTokenExpirationDate || null);
+
+		if (loading) {
+			setLoading(false);
+		}
+	};
+
+	const value = useMemo<PopupContext | RedirectContext>(() => {
+		if (!sdk) {
+			sdk = initFlow(options);
+		}
+
+		return {
+			loading,
+			isAuthenticated,
+			idTokenClaims,
+			accessToken,
+			refreshToken,
+			accessTokenExpired,
+			accessTokenExpirationDate,
+
+			login: async (options?: Parameters<PopupFlow['login'] | RedirectFlow['login']>[0]) => {
+				await sdk.login(options);
+				await updateSession();
+			},
+			register: async (options?: Parameters<PopupFlow['register'] | RedirectFlow['register']>[0]) => {
+				await sdk.register(options);
+				await updateSession();
+			},
+			refresh: async () => {
+				await sdk.refresh();
+				await updateSession();
+			},
+			revoke: async () => {
+				await sdk.revoke();
+				await updateSession();
+			},
+			logout: async (options?: Parameters<PopupFlow['logout'] | RedirectFlow['logout']>[0]) => {
+				await sdk.logout(options);
+				await updateSession();
+			},
+			handleCallback: async (url?: Parameters<PopupFlow['handleCallback'] | RedirectFlow['handleCallback']>[0]) => {
+				await sdk.handleCallback(url);
+				await updateSession();
+			},
+		};
+	}, [loading, idTokenClaims, accessToken, refreshToken, accessTokenExpired, accessTokenExpirationDate, isAuthenticated]);
+
+	useEffect(() => {
+		if (!sdk) {
+			return;
+		}
+
+		void updateSession();
+
+		sdk.subscribeToEvent('init', updateSession);
+		sdk.subscribeToEvent('loggedIn', updateSession);
+		sdk.subscribeToEvent('sessionLoaded', updateSession);
+		sdk.subscribeToEvent('tokenRefreshed', updateSession);
+		sdk.subscribeToEvent('tokenRefreshFailed', updateSession);
+		sdk.subscribeToEvent('logoutInitiated', updateSession);
+		sdk.subscribeToEvent('tokenRevoked', updateSession);
+		sdk.subscribeToEvent('tokenRevokeFailed', updateSession);
+	}, []);
+
+	return <StrivacitySdk.Provider value={value}>{children}</StrivacitySdk.Provider>;
+};
---
+++ packages/sdk-next/tests/composables.spec.tsx
@@ -0,0 +1,158 @@
+import { vi, describe, it, afterEach, expect } from 'vitest';
+import { type ReactNode } from 'react';
+import { renderHook, waitFor } from '@testing-library/react';
+import { initFlow } from '@strivacity/sdk-core';
+import { RedirectFlow } from '@strivacity/sdk-core/flows/RedirectFlow';
+import { timestamp } from '@strivacity/sdk-core/utils/date';
+
+import { type SDKOptions, type IdTokenClaims, LocalStorage, AuthProvider, useStrivacity } from '../src';
+
+const options: SDKOptions = {
+	issuer: 'https://brandtegrity.io',
+	scopes: ['openid', 'profile'],
+	clientId: '2202c596c06e4774b42804af00c66df9',
+	redirectUri: 'https://brandtegrity.io/app/callback/',
+	responseType: 'code',
+	responseMode: 'query',
+};
+
+// eslint-disable-next-line react/display-name
+const AuthProviderWrapper = ({ children }: { children?: ReactNode }) => <AuthProvider options={options}>{children}</AuthProvider>;
+
+const flow = new RedirectFlow(options, new LocalStorage());
+const subscribeToEventSpy = vi.spyOn(flow, 'subscribeToEvent');
+const isAuthenticatedMock = vi.fn((): Promise<boolean> => Promise.resolve(false));
+const idTokenClaimsMock = vi.fn((): IdTokenClaims | null | undefined => undefined);
+const accessTokenMock = vi.fn((): string | null | undefined => undefined);
+const refreshTokenMock = vi.fn((): string | null | undefined => undefined);
+const accessTokenExpiredMock = vi.fn((): boolean => true);
+const accessTokenExpirationDateMock = vi.fn((): number | null | undefined => undefined);
+
+Object.defineProperty(flow, 'isAuthenticated', { get: isAuthenticatedMock });
+Object.defineProperty(flow, 'idTokenClaims', { get: idTokenClaimsMock });
+Object.defineProperty(flow, 'accessToken', { get: accessTokenMock });
+Object.defineProperty(flow, 'refreshToken', { get: refreshTokenMock });
+Object.defineProperty(flow, 'accessTokenExpired', { get: accessTokenExpiredMock });
+Object.defineProperty(flow, 'accessTokenExpirationDate', { get: accessTokenExpirationDateMock });
+
+vi.mock('@strivacity/sdk-core', () => ({ initFlow: vi.fn() }));
+vi.mocked(initFlow).mockReturnValue(flow);
+
+describe('useStrivacity', () => {
+	afterEach(() => {
+		subscribeToEventSpy.mockClear();
+		isAuthenticatedMock.mockClear();
+		idTokenClaimsMock.mockClear();
+		accessTokenMock.mockClear();
+		refreshTokenMock.mockClear();
+		accessTokenExpiredMock.mockClear();
+		accessTokenExpirationDateMock.mockClear();
+	});
+
+	it('should create and provide sdk instance correctly', async () => {
+		const { result } = renderHook(useStrivacity, { wrapper: AuthProviderWrapper });
+
+		expect(initFlow).toHaveBeenCalledWith(options);
+		expect(subscribeToEventSpy).toHaveBeenCalledWith('loggedIn', expect.any(Function));
+		expect(subscribeToEventSpy).toHaveBeenCalledWith('sessionLoaded', expect.any(Function));
+		expect(subscribeToEventSpy).toHaveBeenCalledWith('tokenRefreshed', expect.any(Function));
+		expect(subscribeToEventSpy).toHaveBeenCalledWith('tokenRefreshFailed', expect.any(Function));
+		expect(subscribeToEventSpy).toHaveBeenCalledWith('logoutInitiated', expect.any(Function));
+		expect(subscribeToEventSpy).toHaveBeenCalledWith('tokenRevoked', expect.any(Function));
+		expect(subscribeToEventSpy).toHaveBeenCalledWith('tokenRevokeFailed', expect.any(Function));
+		expect(result.current).toEqual({
+			loading: true,
+			isAuthenticated: false,
+			idTokenClaims: null,
+			accessToken: null,
+			refreshToken: null,
+			accessTokenExpired: true,
+			accessTokenExpirationDate: null,
+
+			login: expect.anything(),
+			register: expect.anything(),
+			refresh: expect.anything(),
+			revoke: expect.anything(),
+			logout: expect.anything(),
+			handleCallback: expect.anything(),
+		});
+
+		await waitFor(() => !result.current.loading);
+	});
+
+	it('should update context correctly', async () => {
+		const claims = {
+			iss: 'https://brandtegrity.io/',
+			aud: [crypto.randomUUID().replace(/-/g, '')],
+			nonce: crypto.randomUUID().replace(/-/g, ''),
+			auth_time: timestamp(),
+			exp: timestamp(),
+			iat: timestamp(),
+			jti: crypto.randomUUID(),
+			sid: crypto.randomUUID(),
+			sub: crypto.randomUUID(),
+		};
+		const accessToken = 'accessToken';
+		const refreshToken = 'refreshToken';
+		const accessTokenExpirationDate = timestamp() + 3600;
+		const { result } = renderHook(useStrivacity, { wrapper: AuthProviderWrapper });
+
+		expect(result.current.loading).toBeTruthy();
+		await waitFor(() => !flow.isAuthenticated);
+		expect(result.current.idTokenClaims).toBeNull();
+		expect(result.current.accessToken).toBeNull();
+		expect(result.current.refreshToken).toBeNull();
+		expect(result.current.accessTokenExpired).toBeTruthy();
+		expect(result.current.accessTokenExpirationDate).toBeNull();
+
+		isAuthenticatedMock.mockReturnValue(Promise.resolve(true));
+		idTokenClaimsMock.mockReturnValue(claims);
+		accessTokenMock.mockReturnValue(accessToken);
+		refreshTokenMock.mockReturnValue(refreshToken);
+		accessTokenExpiredMock.mockReturnValue(false);
+		accessTokenExpirationDateMock.mockReturnValue(accessTokenExpirationDate);
+		// @ts-expect-error: Protected function
+		flow.dispatchEvent('loggedIn', [{ accessToken, refreshToken, claims }]);
+
+		expect(result.current.loading).toBeFalsy();
+		await waitFor(() => flow.isAuthenticated);
+		expect(result.current.isAuthenticated).toBeTruthy();
+		expect(result.current.idTokenClaims).toEqual(claims);
+		expect(result.current.accessToken).toEqual(accessToken);
+		expect(result.current.refreshToken).toEqual(refreshToken);
+		expect(result.current.accessTokenExpired).toBeFalsy();
+		expect(result.current.accessTokenExpirationDate).toEqual(accessTokenExpirationDate);
+	});
+
+	it('should call sdk functions correctly', async () => {
+		const spies = {
+			login: vi.spyOn(flow, 'login').mockReturnValue(Promise.resolve()),
+			register: vi.spyOn(flow, 'register').mockReturnValue(Promise.resolve()),
+			refresh: vi.spyOn(flow, 'refresh').mockReturnValue(Promise.resolve()),
+			revoke: vi.spyOn(flow, 'revoke').mockReturnValue(Promise.resolve()),
+			logout: vi.spyOn(flow, 'logout').mockReturnValue(Promise.resolve()),
+			handleCallback: vi.spyOn(flow, 'handleCallback').mockReturnValue(Promise.resolve()),
+		};
+		const { result } = renderHook(useStrivacity, { wrapper: AuthProviderWrapper });
+
+		await waitFor(() => !flow.isAuthenticated);
+
+		result.current.login({ loginHint: 'login' });
+		result.current.register({ loginHint: 'register' });
+		result.current.refresh();
+		result.current.revoke();
+		result.current.logout({ postLogoutRedirectUri: 'uri' });
+		result.current.handleCallback();
+
+		expect(spies.login).toHaveBeenCalledWith({ loginHint: 'login' });
+		expect(spies.register).toHaveBeenCalledWith({ loginHint: 'register' });
+		expect(spies.refresh).toHaveBeenCalled();
+		expect(spies.revoke).toHaveBeenCalled();
+		expect(spies.logout).toHaveBeenCalledWith({ postLogoutRedirectUri: 'uri' });
+		expect(spies.handleCallback).toHaveBeenCalled();
+	});
+
+	it('should throw error without AuthProvider', () => {
+		expect(() => renderHook(useStrivacity)).toThrowError('Missing Strivacity SDK context');
+	});
+});
