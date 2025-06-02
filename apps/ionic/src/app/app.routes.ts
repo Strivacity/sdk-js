6d66462e2caf66512ce808c2943ccb24bc2d9ed5
Istvan Ujj-Meszaros <istvan.ujj-meszaros@strivacity.com>
2025-06-02 14:06:11 +0200

    feat(ionic): good DOM, invisible content (hidden by the header)

--- apps/ionic/src/app/app.routes.ts
+++ apps/ionic/src/app/app.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 import { HomePage } from './pages/home';
 
 export const routes: Routes = [
-  { path: '', component: HomePage },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+  },
   {
     path: '',
     redirectTo: 'home',
