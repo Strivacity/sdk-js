93eaba163bdc206e5199cec9c2a2a72f22eea3be
Sezer <sezer.dogru91@gmail.com>
2025-08-28 09:33:30 +0200

    feat: webauthn nuxt widget example added

--- apps/nuxt/app/utils/widgets.ts
+++ apps/nuxt/app/utils/widgets.ts
@@ -1,18 +1,19 @@
-import type { Component } from 'vue';
 import type { WidgetType } from '@strivacity/sdk-core';
+import type { Component } from 'vue';
 
 import CheckboxWidget from '../components/checkbox.widget.vue';
 import DateWidget from '../components/date.widget.vue';
 import InputWidget from '../components/input.widget.vue';
 import LayoutWidget from '../components/layout.widget.vue';
-import MultiSelectWidget from '../components/multiselect.widget.vue';
 import LoadingWidget from '../components/loading.widget.vue';
+import MultiSelectWidget from '../components/multiselect.widget.vue';
 import PasscodeWidget from '../components/passcode.widget.vue';
 import PasswordWidget from '../components/password.widget.vue';
 import PhoneWidget from '../components/phone.widget.vue';
 import SelectWidget from '../components/select.widget.vue';
 import StaticWidget from '../components/static.widget.vue';
 import SubmitWidget from '../components/submit.widget.vue';
+import WebauthnWidget from '../components/webauthn.widget.vue';
 
 export const widgets: Record<WidgetType, Component> = {
 	checkbox: CheckboxWidget,
@@ -27,4 +28,5 @@ export const widgets: Record<WidgetType, Component> = {
 	multiSelect: MultiSelectWidget,
 	static: StaticWidget,
 	submit: SubmitWidget,
+	webauthn: WebauthnWidget,
 };
