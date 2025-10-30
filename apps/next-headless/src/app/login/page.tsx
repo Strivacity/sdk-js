7f4a74738ec99a94501bd7ad70719258b3de8b75
Péter Gaál <peter.gaal@strivacity.com>
2025-10-30 12:13:03 +0100

    feat: redirect to the home page on OIDC error

--- apps/next-headless/src/app/login/page.tsx
+++ apps/next-headless/src/app/login/page.tsx
@@ -118,6 +118,7 @@ export default function Login() {
 				window.location.href = state.hostedUrl;
 			} else {
 				alert(error.message);
+				window.location.href = '/';
 			}
 		} finally {
 			setIsLoading(false);
