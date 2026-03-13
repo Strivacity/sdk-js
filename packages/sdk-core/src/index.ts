17812c641b885c3babf97981a2d24d5579e30da2
Péter Gaál <peter.gaal@strivacity.com>
2026-03-13 13:51:20 +0100

    feat: custom sdk mode added

--- packages/sdk-core/src/index.ts
+++ packages/sdk-core/src/index.ts
@@ -1,4 +1,5 @@
 import type { SDKOptions } from './types';
+import { BaseFlow } from './flows/BaseFlow';
 import { RedirectFlow } from './flows/RedirectFlow';
 import { PopupFlow } from './flows/PopupFlow';
 import { NativeFlow } from './flows/NativeFlow';
@@ -20,8 +21,13 @@ export function initFlow(options: SDKOptions & { mode: 'popup' }): PopupFlow;
 export function initFlow(options: SDKOptions & { mode: 'redirect' }): RedirectFlow;
 export function initFlow(options: SDKOptions & { mode: 'native' }): NativeFlow;
 export function initFlow(options: SDKOptions & { mode: 'embedded' }): EmbeddedFlow;
-export function initFlow(options: SDKOptions & { mode?: 'popup' | 'redirect' | 'native' | 'embedded' }): PopupFlow | RedirectFlow | NativeFlow | EmbeddedFlow;
-export function initFlow(options: SDKOptions & { mode?: 'popup' | 'redirect' | 'native' | 'embedded' }): PopupFlow | RedirectFlow | NativeFlow | EmbeddedFlow {
+export function initFlow(options: SDKOptions & { mode: 'custom' }): EmbeddedFlow;
+export function initFlow(
+	options: SDKOptions & { mode?: 'popup' | 'redirect' | 'native' | 'embedded' | 'custom' },
+): PopupFlow | RedirectFlow | NativeFlow | EmbeddedFlow | typeof options.customFlow;
+export function initFlow(
+	options: SDKOptions & { mode?: 'popup' | 'redirect' | 'native' | 'embedded' | 'custom' },
+): PopupFlow | RedirectFlow | NativeFlow | EmbeddedFlow | typeof options.customFlow {
 	const StorageClass = options.storage || LocalStorage;
 	const HttpClientClass = options.httpClient || HttpClient;
 
@@ -40,6 +46,12 @@ export function initFlow(options: SDKOptions & { mode?: 'popup' | 'redirect' | '
 		return new NativeFlow(options, storage, httpClient, logging);
 	} else if (options.mode === 'embedded') {
 		return new EmbeddedFlow(options, storage, httpClient, logging);
+	} else if (options.mode === 'custom') {
+		if (!options.customFlow || !(options.customFlow.prototype instanceof BaseFlow)) {
+			throw new Error('customFlow must extend BaseFlow');
+		}
+
+		return new options.customFlow(options, storage, httpClient, logging) as unknown as typeof options.customFlow;
 	} else {
 		return new RedirectFlow(options, storage, httpClient, logging);
 	}
