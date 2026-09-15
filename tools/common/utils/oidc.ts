304639a4382857acdca9274a5f4ad67f956cd843
Péter Gaál <peter.gaal@strivacity.com>
2026-09-15 09:53:07 +0200

    fix: process.env handling fixed

--- tools/common/utils/oidc.ts
+++ tools/common/utils/oidc.ts
@@ -1,10 +1,14 @@
 import type { ExtraRequestArgs } from '@strivacity/sdk-core/types';
 
+declare const process: { env?: Record<string, string | undefined> } | undefined;
+
 export function getExtraParams(extraArgs?: ExtraRequestArgs): ExtraRequestArgs {
+	const env = typeof process !== 'undefined' ? process?.env : undefined;
+
 	return {
-		loginHint: import.meta.env?.VITE_LOGIN_HINT ?? process?.env?.LOGIN_HINT,
-		acrValues: import.meta.env?.VITE_ACR_VALUES?.split(' ') ?? process?.env?.ACR_VALUES?.split(' '),
-		audiences: import.meta.env?.VITE_AUDIENCES?.split(' ') ?? process?.env?.AUDIENCES?.split(' '),
+		loginHint: import.meta.env?.VITE_LOGIN_HINT ?? env?.LOGIN_HINT,
+		acrValues: import.meta.env?.VITE_ACR_VALUES?.split(' ') ?? env?.ACR_VALUES?.split(' '),
+		audiences: import.meta.env?.VITE_AUDIENCES?.split(' ') ?? env?.AUDIENCES?.split(' '),
 		...extraArgs,
 	};
 }
